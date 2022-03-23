3f22c0ae16fb8022a0bec4c4cd9f640d8f0eb8de
P Paul Jonathan <ppauljonathan@gmail.com>
2022-03-23 23:04:52 +0530

    added db insert logic

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Video=require('../models/video');
 
 module.exports.getMain=async(req,res,next)=>{
@@ -14,11 +15,37 @@ module.exports.getSermon=(req,res,next)=>{
 
 module.exports.getUpload=(req,res,next)=>{
     res.render('upload',{
-        title:'Upload Sermon'
+        title:'Upload Sermon',
+        errors:[]
     })
 }
 
-module.exports.postUpload=(req,res,next)=>{
-    console.log(req.body);
+module.exports.postUpload=async(req,res,next)=>{
+    if(
+        req.body.title.trim().length==0||
+        req.body.link.trim().length==0||
+        req.body.category.trim().length==0||
+        req.body.speaker.trim().length==0||
+        req.body.date.trim().length==0||
+        req.body.password.trim().length==0
+    ){
+        res.render('upload',{
+            title:'Upload Sermon',
+            errors:["Fields Must Not Be Empty"]
+        })
+    }else if(req.body.password.trim()!=process.env.PASSWORD){
+        res.render('upload',{
+            title:'Upload Sermon',
+            errors:["Password Incorrect"]
+        })
+    }else{
+        await Video.create({
+            title:req.body.title.trim(),
+            category:req.body.category.trim(),
+            speaker:req.body.speaker.trim(),
+            link:req.body.link.trim(),
+            date:req.body.date.trim()
+        })
     res.redirect('/upload');
+    }
 }
--- models/video.js
+++ models/video.js
@@ -18,7 +18,7 @@ const videoSchema=new Schema({
         type:String,
         required:true
     },
-    url:{
+    link:{
         type:String,
         required:true
     }
